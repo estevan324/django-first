3bb42dcfb94a0e4b493fb8ef0156e8bceac0493e
Estevan Hernandes Silva de Oliveirs <estevanhernandes1@gmail.com>
2023-07-13 20:24:38 -0300

    Projeto finalizado

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    index,
+    contact,
+    produto
+)
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contact/', contact, name='contato'),
+    path('produto/<int:pk>', produto, name='produto')
+]
